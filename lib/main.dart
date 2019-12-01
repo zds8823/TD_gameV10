431ff30c6e0e7e7972d917a7976782c33b310542
zds8823 <36308993+zds8823@users.noreply.github.com>
2019-12-01 16:24:52 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/home_page.dart';
+import 'package:flutter/services.dart';
 //import 'package:flame/game.dart';
 import 'dart:ui';
 import 'package:flutter/gestures.dart';
@@ -10,8 +11,12 @@ void main() async => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return new MaterialApp(
-      theme: new ThemeData(primaryColor: Colors.black),
+      theme: new ThemeData(primaryColor: Colors.blue),
       home: new HomePage(),
     );
   }
